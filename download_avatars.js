e2ac356185b29f131bd7434d610019a31bfae3dd
Scott Veale <srveale42@gmail.com>
2017-01-16 16:35:54 +0000

    added downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -40,3 +41,17 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(user.avatar_url);
   })
 });
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Downloading File');
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg");
