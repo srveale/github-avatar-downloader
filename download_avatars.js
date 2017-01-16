1b39951b495fcbea9e5cb2b3052b7d6e1e2f2b7f
Scott Veale <srveale42@gmail.com>
2017-01-16 17:14:54 +0000

    implemented command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,13 @@
 var request = require('request');
 var fs = require('fs')
 
+
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+if (!repoOwner || !repoName) {
+  throw "Program requires Owner and Name arguments";
+}
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
@@ -26,32 +33,38 @@ function getRepoContributors(repoOwner, repoName, cb) {
          .on('response', function (response) {
          })
 
-         //.pipe(fs.createWriteStream('./future.html'))
-
          .on('end', function(){
           console.log('Finished downloading');
          });
   return
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+
+       .on('error', function (err) {
+         throw err;
+       })
+
+       .on('response', function (response) {
+         console.log('Downloading File');
+       })
+
+       .pipe(fs.createWriteStream(filePath));
+}
+
+getRepoContributors(repoOwner, repoName, function(err, result) {
+
   console.log("Errors:", err);
+
   JSON.parse(result.body).forEach(function (user) {
+
     var filePath = './avatars/' + user.login + '.jpg';
     downloadImageByURL(user.avatar_url, filePath);
+
   })
 });
 
 
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Downloading File');
-       })
-       .pipe(fs.createWriteStream(filePath));
-}
 
-downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', './avatars/kvirani.jpg');
+
