c65bef94df8c28fb012ec5f00316cf84f5ffbd76
Scott Veale <srveale42@gmail.com>
2017-01-16 15:35:12 +0000

    made request using url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,38 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var GITHUB_USER = "YOUR USERNAME HERE";
+  var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+
+  request.get(requestURL)
+
+         .on('error', function (err) {
+           throw err;
+         })
+
+         .on('response', function (response) {
+           console.log(response);
+         })
+
+         //.pipe(fs.createWriteStream('./future.html'))
+
+         .on('end', function(){
+          console.log('Finished downloading');
+         });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
